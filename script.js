e5e33777db75f5ef11502a3649a90a82b29f753c
Ayush Rathi <ayushrathi982@gmail.com>
2020-12-29 19:01:10 +0530

    Minor bug fixes: Second Commit

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var list1= new fun('coding','20/10/20',1);
 project1.pro.push(list1);
 
 function display(name) {
-	var txt="<tr><td class='project_name'><div class='btn btn-primary input' onclick='clickonprojects(event)'>"+name+"</div></td><td class='project_button'><div class='btn btn-primary' onclick='del_project(event)' id='input'><i class='fas fa-trash-alt'></i></div></td></tr>";
+	var txt="<tr><td class='project_name'><div class='btn btn-primary input' onclick='clickonprojects(event)'>"+name+"</div></td><td class='project_button'><div class='btn btn-primary' id='input'><i class='fas fa-trash-alt' onclick='del_project(event)'></i></div></td></tr>";
 	document.getElementById('nm').innerHTML+=txt;
 }
 function add_project() {
@@ -74,29 +74,30 @@ function add_list() {
 function display_list(i) {
 	var x=arr[i].pro;
 	for(var j=0;j<x.length;j++){
-		var msg="<tr><td>"+x[j].list+"</td><td>"+x[j].date+"</td><td>"+x[j].priority+"</td><td><div class='btn btn-primary' onclick='del_lists(event)'><i class='fas fa-trash-alt'></i></div></td></tr>";
+		var msg="<tr><td>"+x[j].list+"</td><td>"+x[j].date+"</td><td>"+x[j].priority+"</td><td><div class='btn btn-primary'><i class='fas fa-trash-alt' onclick='del_lists(event)'></i></div></td></tr>";
 		document.getElementById('inf').innerHTML+=msg;
 	}
 }
 function del_project(event) {
-	var node= event.target;
-	var txt=node.textContent;
+	var node = event.target.parentElement;
+	var txt = node.parentElement.parentElement.childNodes[0].childNodes[0].textContent;
 	for(var i=0;i<arr.length;i++){
-		if(arr[i].name===txt){
-			arr.slice(i);
+		if(arr[i].name === txt){
+			arr.splice(i);
 			break;
 		}
 	}
 	node.parentElement.parentElement.remove();
 	document.getElementById('inf').innerHTML="";
+	console.log(arr);
 }
 function del_lists(event) {
-	var node=event.target;
+	var node=event.target.parentElement;
 	var x= node.parentElement.parentElement.childNodes[0].textContent;
 	var y= arr[active_project].pro;
 	for(var j=0;j<y.length;j++){
-		if(y[j].priority===x){
-			y.slice(j);
+		if(y[j].list===x){
+			y.splice(j);
 			break;
 		}
 	}
